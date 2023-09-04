36c0ef0d0afbe7faba492acca9dada4e83fa2648
NinaHerasymova <predvoditeldvoryanstva@gmail.com>
2023-09-04 17:25:21 +0300

    Add hero section component

---
+++ components/Main/HeroBanner.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center mt-10">
+    <img
+      src="/img/cards.jpg"
+      alt="Fidgrove Features"
+      class="lg:w-4/6 lg:h-96 object-cover"
+    />
+    <div class="my-16 max-w-screen-md">
+      <p
+        class="pb-4 text-left"
+        v-text="
+          'Fidgrove is data management and data analysis at its best, leveraging on the latest relevant technology wrapped in a cloud-based user friendly solution.'
+        "
+      />
+      <p
+        class="text-left"
+        v-text="
+          'Fidgrove aims to help you improve driving performance, get to optimized setups quickly, and support you through racing events. We continuously adding features to get closer to accomplish this mission. For the casual sim racer and professional esports teams alike.'
+        "
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container mx-auto px-4 mt-6"><AppPricing /></div>
+  <div class="container mx-auto px-4">
+    <MainHeroBanner /><MainPricingSection />
+  </div>
 </template>
 
 <style scoped></style>
