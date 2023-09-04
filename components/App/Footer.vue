8baccaf6d096d4cfe82061cba7ab09d3a7896d08
NinaHerasymova <predvoditeldvoryanstva@gmail.com>
2023-09-04 17:32:33 +0300

    Update footer styles

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -4,10 +4,10 @@ const year = computed(() => new Date().getFullYear());
 
 <template>
   <div class="bg-black py-6">
-    <div class="container mx-auto">
+    <div class="container mx-auto px-4">
       <div class="flex justify-between items-center">
         <NuxtLink to="/">
-          <img src="/img/logo.png" alt="Fidgrove" class="w-60" />
+          <img src="/img/logo.png" alt="Fidgrove" class="w-40" />
         </NuxtLink>
         <p class="text-white">© Fidgrove, {{ year }}</p>
       </div>
