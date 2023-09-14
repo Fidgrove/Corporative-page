425cdbc4b22e5a5c43629be5e6f73c43f2459e2f
NinaHerasymova <predvoditeldvoryanstva@gmail.com>
2023-09-14 19:08:14 +0300

    Add ms argument to time convertor composable

--- composables/timeFormatting.ts
+++ composables/timeFormatting.ts
@@ -1,10 +1,15 @@
-export const useSecondsToReadableTime = (duration: number) => {
+export const useSecondsToReadableTime = (duration: number, ms = true) => {
   const hours = Math.floor(duration / 3600);
   const minutes = Math.floor((duration % 3600) / 60);
   const seconds = Math.floor(duration) % 60;
-
+  const milliseconds = Math.round(
+    +`.${duration.toString().split(".")[1]}` * 1000,
+  );
   const formattedHours = hours.toString().padStart(2, "0");
   const formattedMinutes = minutes.toString().padStart(2, "0");
   const formattedSeconds = seconds.toString().padStart(2, "0");
-  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  const formattedMilliseconds = milliseconds.toString().padStart(3, "0");
+  return ms
+    ? `${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`
+    : `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 };
