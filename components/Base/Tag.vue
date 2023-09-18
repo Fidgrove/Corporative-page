3a42ead94fb7cd6c5e74260a945d514b5bdbd22b
NinaHerasymova <predvoditeldvoryanstva@gmail.com>
2023-09-18 11:58:09 +0300

    Create base tag component

---
+++ components/Base/Tag.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface tagProps {
+  title: string;
+  value: string;
+}
+
+const props = defineProps<tagProps>();
+</script>
+
+<template>
+  <div class="flex overflow-hidden text-xs text-white">
+    <span class="rounded-l bg-black px-2 py-1" v-text="title" />
+    <span class="rounded-r bg-blue px-2 py-1" v-text="value" />
+  </div>
+</template>
